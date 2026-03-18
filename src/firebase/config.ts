25382c9953625c72f554c2c10b49bbd12ba4d03b
Vipin Mishra <vpn.mishra12@gmail.com>
2026-03-18 11:35:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4253837572-72734",
+  "appId": "1:402138584119:web:d97c8ae4390f61fbae9bb2",
+  "apiKey": "AIzaSyCILAsaSiXFnsWocpO-RPMUh-X-D1apAYM",
+  "authDomain": "studio-4253837572-72734.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "402138584119"
+};
